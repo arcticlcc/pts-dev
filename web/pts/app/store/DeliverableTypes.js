79a0784179f94c25b5dd37347b3faebfd47f322b
Josh Bradley <jbradley@arcticlcc.org>
2012-05-09 19:36:17 -0800

    Removed TODO

--- web/pts/app/store/DeliverableTypes.js
+++ web/pts/app/store/DeliverableTypes.js
@@ -1,8 +1,7 @@
-/*
- * File: app/store/DeliverableTypes.js
+/**
  * Description: Store of Deliverable Types
  */
-//TODO: create separate store for tasks
+
 Ext.define('PTS.store.DeliverableTypes', {
     extend: 'Ext.data.Store',
     model: 'PTS.model.DeliverableType',
