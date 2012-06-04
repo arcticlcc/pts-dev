2ce15fca55805aa2d54bf9093f762038b7a6d385
Josh Bradley <jbradley@arcticlcc.org>
2012-06-04 09:59:58 -0800

    Create cvl schema for domain tables

--- web/pts/docs/schema/updates.sql
+++ web/pts/docs/schema/updates.sql
@@ -4,7 +4,7 @@ ALTER TABLE costcode DROP CONSTRAINT contactcostcode_costcode_fk;
 
 -- View: projectlist
 
-CREATE OR REPLACE VIEW projectlist AS 
+CREATE OR REPLACE VIEW projectlist AS
  SELECT DISTINCT project.projectid, project.orgid, form_projectcode(project.number::integer, project.fiscalyear::integer, contactgroup.acronym) AS projectcode,
   project.title, project.parentprojectid, project.fiscalyear, project.number, project.startdate, project.enddate, project.uuid,
    COALESCE(sum(funding.amount) OVER (PARTITION BY project.projectid), 0.00) AS allocated,
@@ -19,4 +19,35 @@ CREATE OR REPLACE VIEW projectlist AS
    JOIN funding USING (fundingid)
    JOIN modification USING (modificationid)
   WHERE funding.fundingtypeid = 1
-  GROUP BY modification.projectid) invoice USING (projectid);  
+  GROUP BY modification.projectid) invoice USING (projectid);
+
+-- move domain tables to new schema
+
+-- Schema: cvl
+
+-- DROP SCHEMA cvl;
+
+CREATE SCHEMA cvl
+  AUTHORIZATION bradley;
+
+GRANT ALL ON SCHEMA cvl TO bradley;
+GRANT USAGE ON SCHEMA cvl TO pts_read;
+
+ALTER DATABASE pts SET search_path=pts, cvl, public
+
+ALTER TABLE contacttype SET SCHEMA cvl;
+ALTER TABLE country SET SCHEMA cvl;
+ALTER TABLE deliverabletype SET SCHEMA cvl;
+ALTER TABLE eaddresstype SET SCHEMA cvl;
+ALTER TABLE filetype SET SCHEMA cvl;
+ALTER TABLE format SET SCHEMA cvl;
+ALTER TABLE fundingtype SET SCHEMA cvl;
+ALTER TABLE govunit SET SCHEMA cvl;
+ALTER TABLE modcontacttype SET SCHEMA cvl;
+ALTER TABLE modtype SET SCHEMA cvl;
+ALTER TABLE modtypestatus SET SCHEMA cvl;
+ALTER TABLE phonetype SET SCHEMA cvl;
+ALTER TABLE postalcode SET SCHEMA cvl;
+ALTER TABLE position SET SCHEMA cvl;
+ALTER TABLE roletype SET SCHEMA cvl;
+ALTER TABLE status SET SCHEMA cvl;
