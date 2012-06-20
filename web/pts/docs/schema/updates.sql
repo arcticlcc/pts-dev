79cdc9d41e805f5cc7b374b179b99e96b1c68e1f
Josh Bradley <jbradley@arcticlcc.org>
2012-06-19 16:28:46 -0800

    Update comments tables.

--- web/pts/docs/schema/updates.sql
+++ web/pts/docs/schema/updates.sql
@@ -1,47 +1,41 @@
--- View: deliverabledue
+ALTER TABLE pts.modcomment ADD COLUMN datemodified DATE NOT NULL;
 
--- DROP VIEW deliverabledue;
+ALTER TABLE pts.modcomment ADD COLUMN modcommentid SERIAL;
 
-CREATE OR REPLACE VIEW deliverabledue AS
- SELECT DISTINCT ON (dm.duedate, d.deliverableid) dm.duedate,receiveddate, d.title, d.description, projectlist.projectcode, project.shorttitle AS project,
-  (personlist.firstname::text || ' '::text) || personlist.lastname::text AS contact, personlist.priemail AS email,
-   CASE WHEN receiveddate IS NOT NULL THEN 0 ELSE 'now'::text::date - dm.duedate END AS dayspastdue,
-    modification.projectid, dm.modificationid, d.deliverableid
-   FROM deliverable d
-   JOIN deliverablemod dm USING (deliverableid)
-   JOIN modification USING (modificationid)
-   JOIN projectlist USING (projectid)
-   JOIN project USING (projectid)
-   LEFT JOIN (SELECT * from projectcontact WHERE roletypeid IN (6,7)) as projectcontact USING (projectid)
-   LEFT JOIN personlist USING (contactid)
-  WHERE NOT dm.invalid OR NOT (EXISTS ( SELECT 1
-      FROM deliverablemod dp
-     WHERE dm.modificationid = dp.parentmodificationid AND dm.deliverableid = dp.parentdeliverableid))
-  ORDER BY dm.duedate, d.deliverableid, roletypeid, projectcontact.priority;
+ALTER TABLE pts.modcomment DROP CONSTRAINT modcomment_pk;
 
-ALTER TABLE deliverabledue
-  OWNER TO bradley;
-GRANT ALL ON TABLE deliverabledue TO bradley;
-GRANT SELECT ON TABLE deliverabledue TO pts_read;
+ALTER TABLE pts.modcomment ADD PRIMARY KEY (modcommentid);
 
--- View: task
+ALTER TABLE pts.filecomment ADD COLUMN datemodified DATE NOT NULL;
 
- DROP VIEW task;
+ALTER TABLE pts.filecomment ADD COLUMN filecommentid SERIAL;
 
-CREATE OR REPLACE VIEW task AS
- SELECT deliverablemod.duedate, deliverable.title, deliverablemod.receiveddate, (person.firstname::text || ' '::text) || person.lastname::text AS assignee, deliverable.description,
-deliverablemod.deliverableid, person.contactid, projectid, modificationid
-   FROM deliverablemod
-   JOIN deliverable USING (deliverableid)
-   JOIN person ON deliverablemod.personid = person.contactid
-   join modification using (modificationid)
-join projectlist using (projectid)
-  WHERE (deliverable.deliverabletypeid = ANY (ARRAY[4, 7])) AND (NOT deliverablemod.invalid OR NOT (EXISTS ( SELECT 1
-   FROM deliverablemod dm
-  WHERE deliverablemod.modificationid = dm.parentmodificationid AND deliverablemod.deliverableid = dm.parentdeliverableid)))
-  ORDER BY deliverablemod.duedate DESC;
+ALTER TABLE pts.filecomment DROP CONSTRAINT filecomment_pkey;
 
-ALTER TABLE task
-  OWNER TO bradley;
-GRANT ALL ON TABLE task TO bradley;
-GRANT SELECT ON TABLE task TO pts_read;
+ALTER TABLE pts.filecomment ADD PRIMARY KEY (filecommentid);
+
+ALTER TABLE pts.projectcomment ADD COLUMN datemodified DATE NOT NULL;
+
+ALTER TABLE pts.projectcomment ADD COLUMN projectcommentid SERIAL;
+
+ALTER TABLE pts.projectcomment DROP CONSTRAINT projectcomment_pk;
+
+ALTER TABLE pts.projectcomment ADD PRIMARY KEY (projectcommentid);
+
+ALTER TABLE pts.deliverablecomment ADD COLUMN deliverablecommentid SERIAL;
+
+ALTER TABLE pts.deliverablecomment ADD COLUMN datemodified DATE NOT NULL;
+
+ALTER TABLE pts.deliverablecomment DROP CONSTRAINT deliverablecomment_pk;
+
+ALTER TABLE pts.deliverablecomment ADD PRIMARY KEY (deliverablecommentid);
+
+ALTER TABLE pts.invoicecomment ADD COLUMN invoicecommentid SERIAL;
+
+ALTER TABLE pts.invoicecomment ADD COLUMN datemodified DATE NOT NULL;
+
+ALTER TABLE pts.invoicecomment DROP CONSTRAINT invoicecomment_pk;
+
+ALTER TABLE pts.invoicecomment ADD PRIMARY KEY (invoicecommentid);
+
+COMMENT ON COLUMN deliverablecomment.datemodified IS 'Date that the comment was modified.';
