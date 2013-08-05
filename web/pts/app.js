edd0cdaff34d36a36893cd4b230c4d71dac86d2c
Josh Bradley <jbradley@arcticlcc.org>
2013-08-05 15:42:21 -0800

    Production v0.9.3

--- web/pts/app.js
+++ web/pts/app.js
@@ -31,7 +31,7 @@ Ext.application({
         'Ext.ux.window.Notification'
     ],
     name: 'PTS',
-    version: '0.9.2',
+    version: '0.9.3',
 
     appFolder: 'app',
     autoCreateViewport: false,
