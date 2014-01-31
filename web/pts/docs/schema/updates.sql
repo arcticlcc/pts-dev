1f449c60ad3f120465f219f97280ce6246e0a9c7
Josh Bradley <jbradley@arcticlcc.org>
2014-01-30 15:34:14 -0900

    Add shorttitle to modificationlist view

--- web/pts/docs/schema/updates.sql
+++ web/pts/docs/schema/updates.sql
@@ -1,4 +1,6 @@
 -- Version 0.11.1
+-- Version 0.11.2
+
 -- View: modificationlist
 
 -- DROP VIEW modificationlist;
@@ -6,7 +8,7 @@
 CREATE OR REPLACE VIEW modificationlist AS
  SELECT m.modificationid, m.projectid, m.personid, m.modtypeid, m.title, m.description, m.modificationcode, m.effectivedate, m.startdate, m.enddate, m.datecreated, m.parentmodificationid, mt.type AS typetext, mt.code AS typecode, (EXISTS ( SELECT 1
            FROM modification mod
-          WHERE m.modificationid = mod.parentmodificationid)) AS ismodified, p.modificationcode AS parentcode
+          WHERE m.modificationid = mod.parentmodificationid)) AS ismodified, p.modificationcode AS parentcode, m.shorttitle
    FROM modification m
    JOIN modtype mt USING (modtypeid)
    LEFT JOIN modification p ON p.modificationid = m.parentmodificationid;
