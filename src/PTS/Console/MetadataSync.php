6ef0da63c383c098a7317cc84d4ce23706fc8566
Josh Bradley <jbradley@arcticlcc.org>
2015-12-22 16:56:10 -0900

    Improve metadata support

--- src/PTS/Console/MetadataSync.php
+++ src/PTS/Console/MetadataSync.php
@@ -13,12 +13,10 @@ class MetadataSync extends \Knp\Command\Command {
         $this
             ->setName("metadata:sync")
             ->setDescription("Sync the metadata publication database.")
-            ->addOption(
-                'schema',
-                's',
-                InputOption::VALUE_REQUIRED,
-                'Which schema to use? "dev" is the default.',
-                'dev'
+            ->addArgument(
+                'schemas',
+                InputArgument::IS_ARRAY | InputArgument::REQUIRED,
+                'Which schema(s) to sync?'
             );
 
     }
@@ -26,42 +24,43 @@ class MetadataSync extends \Knp\Command\Command {
     protected function execute(InputInterface $input, OutputInterface $output) {
 
         $app = $this->getSilexApplication();
-        $schema = $input->getOption('schema');
+        $schemas = $input->getArgument('schemas');
         $adiwg = new ADIwg($app);
 
-        try {
-            if(!isset($app['dbOptions'][$schema])) {
-                Throw new \Exception("No SQLite config found for $schema schema.");
-            }
-
-            $app['idiorm']->setPath($schema);
-            $build = $adiwg->buildMetaDB(FALSE, $schema);
-            $conn = $build ? $build : $app['dbs'][$schema];
-//dump($conn);
-
-            foreach (['project','product'] as $table) {
-                //delete existing records
-                $conn->delete($table, array('groupid' => $schema));
-                $query = $app['idiorm']->getTable($table)
-                    ->where('exportmetadata', TRUE);
-                $items = $query->find_many();
-
-                if($items) {
-                    $method = 'save' . ucfirst($table);
-                    $getId = "{$table}id";
-                    $app['monolog']->addInfo('Found ' . count($items) .
-                        " $table records for $schema schema.");
-                    foreach ($items as $item) {
-                        $adiwg->$method($item->$getId, $schema);
-                    }
-                    $output->writeln('Sync completed.');
-                }else {
-                    $message = "No {$table}(s) found for $schema.";
-                    $app['monolog']->addInfo($message);
+        foreach ($schemas as $schema) {
+            try {
+                if(!isset($app['dbOptions'][$schema])) {
+                    Throw new \Exception("No SQLite config found for $schema schema.");
                 }
+                $app['idiorm']->setPath($schema);
+                $build = $adiwg->buildMetaDB(FALSE, $schema);
+                $conn = $build ? $build : $app['dbs'][$schema];
+
+                foreach (['project','product'] as $table) {
+                    //delete existing records
+                    $conn->delete($table, array('groupid' => $schema));
+                    $query = $app['idiorm']->getTable($table)
+                        ->where('exportmetadata', TRUE);
+                    $items = $query->find_many();
+
+                    if($items) {
+                        $method = 'save' . ucfirst($table);
+                        $getId = "{$table}id";
+                        $app['monolog']->addInfo('Found ' . count($items) .
+                            " $table records for $schema schema.");
+                        foreach ($items as $item) {
+                            $adiwg->$method($item->$getId, $schema);
+                        }
+                        $output->writeln("Sync completed for $schema::$table.");
+                    }else {
+                        $message = "No {$table}(s) found for $schema.";
+                        $app['monolog']->addInfo($message);
+                        $output->writeln($message);
+                    }
+                }
+            } catch (\Exception $exc) {
+                $app['monolog']->addError($exc->getMessage());
             }
-        } catch (\Exception $exc) {
-            $app['monolog']->addError($exc->getMessage());
         }
     }
 
