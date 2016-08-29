73728d28569592209a015218629d71c3b0484388
Josh Bradley <jbradley@arcticlcc.org>
2016-08-29 15:21:06 -0800

    Update projectawardinfo.sql

--- schema/0.19.2/dev/001-projectawardinfo.sql
+++ schema/0.19.2/dev/001-projectawardinfo.sql
@@ -1,5 +1,5 @@
-CREATE OR REPLACE VIEW dev.projectawardinfo AS 
- SELECT form_projectcode(p.number::integer, p.fiscalyear::integer, contactgroup.acronym) AS projectcode,
+CREATE OR REPLACE VIEW dev.projectawardinfo AS
+ SELECT common.form_projectcode(p.number::integer, p.fiscalyear::integer, contactgroup.acronym) AS projectcode,
     p.title,
     modification.modificationcode AS agreementcode,
     (pi.firstname::text || ' '::text) || pi.lastname::text AS piname,
@@ -33,7 +33,7 @@ CREATE OR REPLACE VIEW dev.projectawardinfo AS
    FROM project p
      LEFT JOIN contactgroup ON p.orgid = contactgroup.contactid
      LEFT JOIN modification USING (projectid)
-     LEFT JOIN status ON status.statusid = project_status(p.projectid)
+     LEFT JOIN cvl.status ON status.statusid = common.project_status(p.projectid)
      LEFT JOIN funding USING (modificationid)
      LEFT JOIN projectcontact pc USING (projectcontactid)
      LEFT JOIN projectcontact rpc ON rpc.projectcontactid = funding.fundingrecipientid
@@ -59,10 +59,10 @@ CREATE OR REPLACE VIEW dev.projectawardinfo AS
              JOIN personlist pl USING (contactid)
              JOIN contactgrouplist ON pl.prigroupid = contactgrouplist.contactid
              LEFT JOIN contactcontactgroup ccg ON pl.contactid = ccg.contactid AND ccg.groupid = pl.prigroupid
-             LEFT JOIN "position" USING (positionid)
+             LEFT JOIN cvl."position" USING (positionid)
           WHERE projectcontact.roletypeid = 7) pi ON p.projectid = pi.projectid AND pi.rank = 1 AND rpg.contactids[1] = pi.contactids[1]
      LEFT JOIN address add ON add.contactid = pi.contactid AND add.addresstypeid = 1
-     LEFT JOIN govunit ON govunit.featureid = add.stateid
+     LEFT JOIN cvl.govunit ON govunit.featureid = add.stateid
      LEFT JOIN ( SELECT pl.contactid,
             pl.firstname,
             pl.lastname,
@@ -84,10 +84,9 @@ CREATE OR REPLACE VIEW dev.projectawardinfo AS
              JOIN personlist pl USING (contactid)
              JOIN contactgrouplist ON pl.prigroupid = contactgrouplist.contactid
              LEFT JOIN contactcontactgroup ccg ON pl.contactid = ccg.contactid AND ccg.groupid = pl.prigroupid
-             LEFT JOIN "position" USING (positionid)
+             LEFT JOIN cvl."position" USING (positionid)
           WHERE projectcontact.roletypeid = ANY (ARRAY[5, 13])) fc ON p.projectid = fc.projectid AND fc.rank = 1 AND rpg.contactids[1] = fc.contactids[1]
      LEFT JOIN address fadd ON fadd.contactid = fc.contactid AND fadd.addresstypeid = 1
-     LEFT JOIN govunit fg ON fg.featureid = fadd.stateid
+     LEFT JOIN cvl.govunit fg ON fg.featureid = fadd.stateid
   WHERE modification.parentmodificationid IS NULL AND (modification.modtypeid = ANY (ARRAY[1, 2, 3, 5])) AND status.weight >= 30 AND status.weight <= 60 AND pc.contactid = p.orgid
-  ORDER BY form_projectcode(p.number::integer, p.fiscalyear::integer, contactgroup.acronym);
-
+  ORDER BY common.form_projectcode(p.number::integer, p.fiscalyear::integer, contactgroup.acronym);
