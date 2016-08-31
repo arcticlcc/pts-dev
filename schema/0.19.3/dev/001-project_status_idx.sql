4c8b289517c665795787378bd1bc81f53cd6b9a7
Josh Bradley <jbradley@arcticlcc.org>
2016-08-31 11:26:00 -0800

    Remove project_status_idx
    The project_status function should be marked as VOLATILE and should not
    be used in an index.

---
+++ schema/0.19.3/dev/001-project_status_idx.sql
@@ -0,0 +1,1 @@
+DROP INDEX dev.project_status_idx;
---
+++ schema/0.19.3/updates.sql
@@ -0,0 +1,52 @@
+Begin;
+
+---- System ----
+
+
+SET search_path TO '';
+
+---- Schema: dev ----
+
+
+---- File: 001-project_status_idx.sql ----
+
+SET search_path TO dev, public;
+
+DROP INDEX dev.project_status_idx;
+
+---- Schema: alcc ----
+
+
+---- File: 001-project_status_idx.sql ----
+
+SET search_path TO alcc, public;
+
+DROP INDEX alcc.project_status_idx;
+
+---- Schema: walcc ----
+
+
+---- File: 001-project_status_idx.sql ----
+
+SET search_path TO walcc, public;
+
+DROP INDEX walcc.project_status_idx;
+
+---- Schema: absi ----
+
+
+---- File: 001-project_status_idx.sql ----
+
+SET search_path TO absi, public;
+
+DROP INDEX absi.project_status_idx;
+
+---- Schema: nwb ----
+
+
+---- File: 001-project_status_idx.sql ----
+
+SET search_path TO nwb, public;
+
+DROP INDEX nwb.project_status_idx;
+
