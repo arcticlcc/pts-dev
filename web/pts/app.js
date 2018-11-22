537e63c061b67b9dc50f21e3fdedacc65491d603
Josh Bradley <jbradley@arcticlcc.org>
2018-11-21 15:24:52 -0900

    Bump version to 0.21.1

--- web/pts/app.js
+++ web/pts/app.js
@@ -31,7 +31,7 @@ Ext.application({
         'Ext.ux.window.Notification'
     ],
     name: 'PTS',
-    version: '0.21.0',
+    version: '0.21.1',
 
     appFolder: 'app',
     autoCreateViewport: false,
